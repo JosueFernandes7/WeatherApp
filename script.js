411d9569c753148bbdfb6f0684f31f0257ea7715
Josue Fernandes <josue7mf@hotmail.com>
2023-01-27 12:31:37 -0300

    terminando funcionalidades

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const dataAtual = new Date();
+const dataContainer = document.querySelector('.dataAtual');
+
+const dia = dataAtual.getDate();
+const mes = dataAtual.getMonth() + 1;
+const ano = dataAtual.getYear();
+
+dataContainer.textContent = `${dia}/${mes}/${ano}`
+
 async function getCoordinates(callback) {
   navigator.geolocation.getCurrentPosition((position) => {
     let lat = position.coords.latitude;
@@ -5,6 +14,7 @@ async function getCoordinates(callback) {
     callback({lat, long});
   })
 }
+
 async function getWeather(lat, long) {
   const URL = "https://api.openweathermap.org/data/2.5/weather?lat="
   const ApiKey = "6b4ef5b00c51bf760d49cf97433a9ea7";
@@ -12,22 +22,39 @@ async function getWeather(lat, long) {
   const units = "metric"
   
   const response =  await fetch(`${URL}${lat}&lon=${long}&units=${units}&lang=${lang}&appid=${ApiKey}`)
-    const responseJson = await response.json();
-    console.log(responseJson);
 
+  const responseJson = await response.json();
   const cityName = responseJson.name;
   const description = responseJson.weather[0].description
-  const maxTemperature = responseJson.main.temp;
-  const feelingTemperature = responseJson.main.feels_like;
+  const temperature = responseJson.main.temp;
+  const Thermalsensation = responseJson.main.feels_like;
+
+  createWeatherContainer(cityName, description, temperature, Thermalsensation)
 }
 getCoordinates((coordinates) => {
-   let {lat,long} =  coordinates;
+  const {lat,long} =  coordinates;
   getWeather(lat,long)
 });
 
-function createWeatherContainer(name, description) {
-  let container = document.createElement('div');
-  container.className = name;
-  container.textContent = name;
+function createWeatherElement(item, type, className, textContent) {
+  const element = document.createElement(type);
+  element.className = className;
+  
+  if(textContent) {
+    element.textContent = `${textContent} = ${item} °C`;
+  } else element.textContent = item;
+  
+  return element;
+}
+
+function createWeatherContainer(cityName, cityDescription,temp,Thermalsensation) {
+  
+  const container = createWeatherElement("","div","weather");
+  const nome = createWeatherElement(cityName, 'h1', 'weatherName');
+  const descricao = createWeatherElement(cityDescription, 'p', 'weatherDescription');
+  const temperatura = createWeatherElement(temp, 'h3', 'weatherTemperature',"Temperatura Atual");
+  const sencacaoTermica = createWeatherElement(Thermalsensation, 'h3', 'weatherThermalsensation',"Sensação Térmica");
+
+  container.append(nome,temperatura,sencacaoTermica,descricao)
   document.querySelector('body').append(container)
 }
